558ab42212101d23b3d0827195d1c891ca58c17a
Pohlonenko <Pohlonenko@mail.ru>
2022-12-09 00:54:38 +0300

    First commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  let result = arr1.every(
+    (item, index) => item === arr2[index] && arr1.length === arr2.length
+  );
+  return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users
+    .filter((itemGender) => itemGender.gender === gender)
+    .map((itemAge) => itemAge.age)
+    .reduce((acc, item, index, users) => {
+      acc += item;
+      if (index === users.length - 1) {
+        return acc / users.length;
+      }
+      return acc;
+    }, 0);
+  return result;
 }
